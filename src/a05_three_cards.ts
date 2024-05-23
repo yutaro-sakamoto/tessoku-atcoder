e17aa18a17cd02ec150b2e9dbec86f073666b195
Yutaro Sakamoto <yutaro-sakamoto@yutaro-sakamoto.com>
2024-05-24 00:43:39 +0900

    [Update]: update a05_three_cards.ts

--- src/a05_three_cards.ts
+++ src/a05_three_cards.ts
@@ -4,4 +4,16 @@ const inputs = fs.readFileSync("/dev/stdin", "utf8");
 const nums = inputs.split(/\s/).map(Number);
 const N = nums[0];
 const K = nums[1];
-console.log(`${N} ${K}`);
+
+let count = 0;
+for (let a = 1; a <= N; a++) {
+    let rest = K - a;
+    for (let b = 1; b <= N && b <= rest && a + b <= K; b++) {
+        let c = K - a - b;
+        if (1 <= c && c <= N) {
+            count++;
+        }
+    }
+}
+
+console.log(count);
